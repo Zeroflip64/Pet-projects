5c23073629ad21fec6e6aa054f9578c019b1b63a
Zeroflip64 <122420589+Zeroflip64@users.noreply.github.com>
2023-05-16 15:43:25 +0700

    Update streamlit_1_project.py

--- streamlit_1_project.py
+++ streamlit_1_project.py
@@ -1,2 +1,13 @@
 import streamlit as st
-st.write('Hello')
+import pandas as pd
+
+df=pd.read_csv('/content/drive/MyDrive/учеба/marketing_campaign.csv',sep='\t')
+my_colors=['#730080','#00ab66','#636363','#779f73']
+
+
+
+st.title("DataFrame of this company")
+st.table(df.head())
+
+st.write("Let's create a correlation matrix between the features to determine which features are important")
+
