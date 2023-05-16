a85e82fa4d952b46cc97f572eb99c95e963ab4fe
Zeroflip64 <122420589+Zeroflip64@users.noreply.github.com>
2023-05-16 15:49:33 +0700

    Update streamlit_1_project.py

--- streamlit_1_project.py
+++ streamlit_1_project.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('https://github.com/Zeroflip64/Pet-projects/raw/main/marketing_campaign.csv')
+df = pd.read_csv('https://github.com/Zeroflip64/Pet-projects/raw/main/marketing_campaign.csv',sep='/t)
 my_colors=['#730080','#00ab66','#636363','#779f73']
 
 
